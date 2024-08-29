c39161c3bc331c243e8939a14446fb1a12237365
GiGiDKR <74984020+GiGiDKR@users.noreply.github.com>
2024-08-29 17:08:56 +0200

    1.0.3

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 TEXT_COLOR='\033[34m'
 RESET_COLOR='\033[0m'
 
-# Fonction pour afficher la bannière
 show_banner() {
     clear
     echo -e '\033[44;97m            OhMyObsidian            \033[0m'
@@ -30,15 +29,15 @@ echo -e "${TEXT_COLOR}Installation des dépendances...${RESET_COLOR}"
 pkg update > /dev/null 2>&1 && pkg upgrade -y > /dev/null 2>&1
 pkg install -y git openssh termux-api > /dev/null 2>&1
 
-show_banner
+echo
 echo -e "${TEXT_COLOR}Création des répertoires nécessaires...${RESET_COLOR}"
 mkdir -p /storage/emulated/0/Documents/Repository $HOME/OhMyObsidian
 
-show_banner
+echo
 echo -e "${TEXT_COLOR}Clonage du dépôt OhMyObsidian...${RESET_COLOR}"
 git clone https://github.com/GiGiDKR/OhMyObsidian.git ~/storage/shared/Documents/Repository/OhMyObsidian
 
-show_banner
+echo
 echo -e "${TEXT_COLOR}Copie du script de configuration...${RESET_COLOR}"
 cp "/storage/emulated/0/Documents/Repository/OhMyObsidian/setup" "$HOME/OhMyObsidian/"
 chmod +x "$HOME/OhMyObsidian/setup"
